d42338603d6623454f0cc936032c2aad08eba48c
Navin Ramsaroop <nr292@cornell.edu>
2017-12-03 00:38:02 -0500

    update1

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -20,13 +20,14 @@ export default class HelloWorldSceneAR extends Component {
     super();
     this.state = {
       rotation: [0, 0, 0],
-      text: 'VIRO Media'
-      //  source: [
-      //    require('./resources/coffee_table/CoffeeTable.obj'),
-      //    require('./resources/WoodenWardrobe/Woodenwardrobe.obj')
-      //  ],
-      //  material: ['coffee_table', 'wooden_wardrobe'],
-      //  current_furniture: 0
+      text: 'VIRO Media',
+      source: [
+        require('./resources/coffee_table/CoffeeTable.obj'),
+        require('./resources/RealisticSofa/realisticSofa.obj'),
+        require('./resources/coffee_table/CoffeeTable.obj')
+      ],
+      material: ['coffee_table', 'coffee_table_2', 'coffee_table_3'],
+      current_furniture: 0
     };
 
     // bind 'this' to functions
@@ -51,6 +52,7 @@ export default class HelloWorldSceneAR extends Component {
           position={[0, 0, -1]}
           scale={[0.5, 0.5, 0.5]}
           source={require('./resources/Icons/Plus-Sign.png')}
+          onClick={this._increment_furniture}
         />
 
         <ViroAmbientLight color={'#aaaaaa'} />
@@ -68,10 +70,10 @@ export default class HelloWorldSceneAR extends Component {
           onDrag={() => {}}
         >
           <Viro3DObject
-            //  source={this.state.source[this.state.current_furniture]}
-            //  materials={[this.state.material[this.state.current_furniture]]}
-            materials={['coffee_table']}
-            source={require('./resources/coffee_table/CoffeeTable.obj')}
+            source={this.state.source[this.state.current_furniture]}
+            materials={[this.state.material[this.state.current_furniture]]}
+            //  materials={['coffee_table']}
+            //source={require('./resources/coffee_table/CoffeeTable.obj')}
             position={this.state.position}
             scale={[0.5, 0.5, 0.5]}
             rotation={this.state.rotation}
@@ -96,7 +98,10 @@ export default class HelloWorldSceneAR extends Component {
     }
   }
   _increment_furniture() {
-    this.state.current_furniture = (this.state.current_furniture + 1) % 2;
+    this.setState({
+      current_furniture: (this.state.current_furniture + 1) % 3
+    });
+    return;
   }
   _onLoadEnd() {
     console.log('Coffee Table finished initializing');
@@ -111,10 +116,13 @@ export default class HelloWorldSceneAR extends Component {
 ViroMaterials.createMaterials({
   coffee_table: {
     diffuseTexture: require('./resources/coffee_table/WoodTexture.jpg')
+  },
+  coffee_table_2: {
+    diffuseTexture: require('./resources/Bed/bluefabric.jpg')
+  },
+  coffee_table_3: {
+    diffuseTexture: require('./resources/Bed/mohagony.jpg')
   }
-  //wooden_wardrobe: {
-  //diffuseTexture: require('./resources/WoodenWardrobe/Wood.jpg')
-  //}
 });
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
